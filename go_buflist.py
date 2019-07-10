90da380ec8571e4e805743ecc751678166c0243f
Simmo Saan <simmo.saan@gmail.com>
2019-07-10 21:48:21 +0300

    Convert tabs to spaces

--- go_buflist.py
+++ go_buflist.py
@@ -43,16 +43,16 @@ SCRIPT_LOCALVAR_HIDDEN = "{}_hidden".format(SCRIPT_LOCALVAR)
 IMPORT_OK = True
 
 try:
-	import weechat
+    import weechat
 except ImportError:
-	print("This script must be run under WeeChat.")
-	print("Get WeeChat now at: http://www.weechat.org/")
-	IMPORT_OK = False
+    print("This script must be run under WeeChat.")
+    print("Get WeeChat now at: http://www.weechat.org/")
+    IMPORT_OK = False
 
 SETTINGS = {}
 
 KEYS = {
-	"meta-g": "/%s" % SCRIPT_COMMAND
+    "meta-g": "/%s" % SCRIPT_COMMAND
 }
 
 
@@ -60,112 +60,112 @@ active = False
 
 
 def input_text_changed_cb(data, signal, buffer):
-	if active:
-		input = weechat.buffer_get_string(buffer, 'input')
-		set_localvars(input)
-		weechat.bar_item_update(SCRIPT_BAR_ITEM)
-	return weechat.WEECHAT_RC_OK
+    if active:
+        input = weechat.buffer_get_string(buffer, 'input')
+        set_localvars(input)
+        weechat.bar_item_update(SCRIPT_BAR_ITEM)
+    return weechat.WEECHAT_RC_OK
 
 
 def command_cb(data, buffer, args):
-	global active
-	active = True
-	set_localvars("")
-	weechat.bar_item_update(SCRIPT_BAR_ITEM)
-	return weechat.WEECHAT_RC_OK
+    global active
+    active = True
+    set_localvars("")
+    weechat.bar_item_update(SCRIPT_BAR_ITEM)
+    return weechat.WEECHAT_RC_OK
 
 
 def command_run_input_cb(data, buffer, command):
-	global active
-	input = weechat.buffer_get_string(buffer, 'input')
+    global active
+    input = weechat.buffer_get_string(buffer, 'input')
 
-	if active:
-		if command == "/input return":
-			active = False
-			set_localvars(input)
-			weechat.bar_item_update(SCRIPT_BAR_ITEM)
-			weechat.bar_item_update("input_text")
-			return weechat.WEECHAT_RC_OK_EAT
+    if active:
+        if command == "/input return":
+            active = False
+            set_localvars(input)
+            weechat.bar_item_update(SCRIPT_BAR_ITEM)
+            weechat.bar_item_update("input_text")
+            return weechat.WEECHAT_RC_OK_EAT
 
-		set_localvars(input)
-		weechat.bar_item_update(SCRIPT_BAR_ITEM)
+        set_localvars(input)
+        weechat.bar_item_update(SCRIPT_BAR_ITEM)
 
-	return weechat.WEECHAT_RC_OK
+    return weechat.WEECHAT_RC_OK
 
 
 def bar_item_cb(data, item, window, buffer, extra_info):
-	if active:
-		input = weechat.buffer_get_string(buffer, 'input')
-		input_pos = weechat.buffer_get_integer(buffer, 'input_pos')
+    if active:
+        input = weechat.buffer_get_string(buffer, 'input')
+        input_pos = weechat.buffer_get_integer(buffer, 'input_pos')
 
-		input_with_cursor = input[:input_pos] + "\x19b#" + input[input_pos:]
+        input_with_cursor = input[:input_pos] + "\x19b#" + input[input_pos:]
 
-		# TODO: input scrolling broken even with bar input start color
-		return "Go: \x19b_{}".format(input_with_cursor)
-	else:
-		return ""
+        # TODO: input scrolling broken even with bar input start color
+        return "Go: \x19b_{}".format(input_with_cursor)
+    else:
+        return ""
 
 
 def input_text_display_with_cursor_cb(data, modifier, modifier_data, string):
-	if active:
-		return ""
-	else:
-		return string
+    if active:
+        return ""
+    else:
+        return string
 
 
 def set_localvars(input):
-	buffers = weechat.infolist_get("buffer", "", "*")
-	while weechat.infolist_next(buffers):
-		pointer = weechat.infolist_pointer(buffers, "pointer")
-		name = weechat.infolist_string(buffers, "name")
+    buffers = weechat.infolist_get("buffer", "", "*")
+    while weechat.infolist_next(buffers):
+        pointer = weechat.infolist_pointer(buffers, "pointer")
+        name = weechat.infolist_string(buffers, "name")
 
-		localvar = None
-		localvar_hidden = None
-		if active:
-			if input in name:
-				localvar = "1"
-				localvar_hidden = "0"
-			else:
-				localvar = "0"
-				localvar_hidden = "1"
+        localvar = None
+        localvar_hidden = None
+        if active:
+            if input in name:
+                localvar = "1"
+                localvar_hidden = "0"
+            else:
+                localvar = "0"
+                localvar_hidden = "1"
 
-		buffer_set_localvar(pointer, SCRIPT_LOCALVAR, localvar)
-		buffer_set_localvar(pointer, SCRIPT_LOCALVAR_HIDDEN, localvar_hidden)
+        buffer_set_localvar(pointer, SCRIPT_LOCALVAR, localvar)
+        buffer_set_localvar(pointer, SCRIPT_LOCALVAR_HIDDEN, localvar_hidden)
 
-	weechat.infolist_free(buffers)
+    weechat.infolist_free(buffers)
 
-	weechat.bar_item_update("buflist")
+    weechat.bar_item_update("buflist")
 
 
 def buffer_set_localvar(buffer, localvar, value):
-	if value is not None:
-		weechat.buffer_set(buffer, "localvar_set_{}".format(localvar), value)
-	else:
-		weechat.buffer_set(buffer, "localvar_del_{}".format(localvar), "")
+    if value is not None:
+        weechat.buffer_set(buffer, "localvar_set_{}".format(localvar), value)
+    else:
+        weechat.buffer_set(buffer, "localvar_del_{}".format(localvar), "")
 
 
 if __name__ == "__main__" and IMPORT_OK:
-	if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
-		# weechat.hook_signal("input_search", "input_search_cb", "")
-		weechat.hook_signal("input_text_changed", "input_text_changed_cb", "")
+    if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+        # weechat.hook_signal("input_search", "input_search_cb", "")
+        weechat.hook_signal("input_text_changed", "input_text_changed_cb", "")
 
-		weechat.hook_command(SCRIPT_COMMAND, SCRIPT_DESC,
+        weechat.hook_command(SCRIPT_COMMAND, SCRIPT_DESC,
 """""",  # TODO
 """""",  # TODO
 """""".replace("\n", ""),  # TODO
-		"command_cb", "")
+        "command_cb", "")
 
-		weechat.bar_item_new("(extra)%s" % SCRIPT_BAR_ITEM, "bar_item_cb", "")
+        weechat.bar_item_new("(extra)%s" % SCRIPT_BAR_ITEM, "bar_item_cb", "")
 
-		weechat.hook_command_run("/input *", "command_run_input_cb", "")
-		weechat.hook_modifier("input_text_display_with_cursor", "input_text_display_with_cursor_cb", "")
+        weechat.hook_command_run("/input *", "command_run_input_cb", "")
+        weechat.hook_modifier("input_text_display_with_cursor", "input_text_display_with_cursor_cb", "")
 
-		for option, value in SETTINGS.items():
-			if not weechat.config_is_set_plugin(option):
-				weechat.config_set_plugin(option, value[0])
+        for option, value in SETTINGS.items():
+            if not weechat.config_is_set_plugin(option):
+                weechat.config_set_plugin(option, value[0])
 
-			weechat.config_set_desc_plugin(option, "%s (default: \"%s\")" % (value[1], value[0]))
+            weechat.config_set_desc_plugin(option, "%s (default: \"%s\")" % (value[1], value[0]))
 
-		weechat.key_bind("default", KEYS)
+        weechat.key_bind("default", KEYS)
 
-		set_localvars("")
+        set_localvars("")
